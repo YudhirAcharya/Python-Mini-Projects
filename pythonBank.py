498f11d7304a787cd760a9d07b00ddd680c8ca92
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2024-06-05 21:17:43 +0545

    bank complete

--- pythonBank.py
+++ pythonBank.py
@@ -9,35 +9,46 @@ def deposit():
 
     if amount < 0:
         print("That's not a valid amount")
+        return 0
     else:
-        return 
+        return amount
 
 
 def withdraw():
-    pass
+    amount = float(input("Enter an amount to be withdrawn: "))
 
-balance = 100
-is_running  = True
-
-while is_running:
-    print("Banking Program")
-    print("1. Show Balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
-
-    choice = input("Enter your choice: ")
-    print("-----------------")
-    if choice == "1":
-        show_balance()
-    elif choice == "2":
-        balance += deposit()
-    elif choice == "3":
-        withdraw()
-    elif choice == "4":
-        is_running == False
+    if amount > balance:
+        print("You don't have enough balance")
+        
     else:
-        print("Not Valid")
-    
-    print("-----------------")
+        return amount
 
+
+def main():
+    balance = 0
+    is_running  = True
+
+    while is_running:
+        print("Banking Program")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ")
+        print("-----------------")
+        if choice == "1":
+            show_balance()
+        elif choice == "2":
+            balance += deposit()
+        elif choice == "3":
+            balance -= withdraw()
+        elif choice == "4":
+            is_running == False
+        else:
+            print("Not Valid")
+        
+        print("-----------------")
+
+
+main()
